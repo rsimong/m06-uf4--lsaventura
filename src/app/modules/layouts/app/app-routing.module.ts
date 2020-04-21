d0aa9f8f53e3c1a85dd478075ae9c3feef4a0fb1
Ricard SG <rsimon@mariabarcelona.com>
2020-04-21 17:16:48 +0200

    Import CanDeactivate

--- src/app/modules/layouts/app/app-routing.module.ts
+++ src/app/modules/layouts/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CreateComponent } from '../app/layouts/create/create.component';
 import { GetListComponent } from '../app/layouts/get-list/get-list.component'
 import { ErrNotFoundComponent } from './layouts/errors/err-not-found/err-not-found.component';
 import { UpdateActivityComponent } from './layouts/update-activity/update-activity.component';
+import { CanDeactivateGuard } from 'src/app/core/guards/can-deactivate.guard';
 
 
 
@@ -16,10 +17,12 @@ const routes: Routes = [
   {
     path: 'create',
     component: CreateComponent,
+    canDeactivate: [CanDeactivateGuard]
   },
   {
     path: 'update/:id',
-    component: UpdateActivityComponent
+    component: UpdateActivityComponent,
+    canDeactivate: [CanDeactivateGuard]
   },
   {
     path: '',
